62d5ff9552363077ad0c42988cf72144a5d2da69
Daniel Richard <danielrichard.vs@gmail.com>
2026-03-11 12:25:10 -0300

    Finalizado Projeto Spotify (11/03/2026)

--- projeto-final-spotify/javascript/script.js
+++ projeto-final-spotify/javascript/script.js
@@ -1,6 +1,46 @@
 const setaE = document.getElementById('seta-e');
 const setaD = document.getElementById('seta-d');
+const setaEB = document.getElementById('seta-e-b');
+const setaDB = document.getElementById('seta-d-b');
+
 const listaDeMusica = document.querySelector('.musica-alta');
+const listaDeArtistas = document.querySelector('.artistas-populares');
+
+const entradaSaida = document.querySelector('.carrosel-container');
+const entradaSaida2 = document.querySelector('.carrosel-container-2');
+
+// Entradas e Saídas do mouse
+
+entradaSaida.addEventListener('mouseenter', () => {
+
+    setaE.style.display = 'block';
+    setaD.style.display = 'block';
+    
+});
+
+entradaSaida.addEventListener('mouseleave', () => {
+
+    setaE.style.display = 'none';
+    setaD.style.display = 'none';
+});
+
+
+entradaSaida2.addEventListener('mouseenter', () => {
+
+    setaEB.style.display = 'block';
+    setaDB.style.display = 'block';
+    
+});
+
+entradaSaida2.addEventListener('mouseleave', () => {
+
+     setaEB.style.display = 'none';
+    setaDB.style.display = 'none';
+});
+
+
+// Eventos de Click
+
 
 setaD.addEventListener('click', () => {
 
@@ -11,5 +51,17 @@ setaD.addEventListener('click', () => {
 setaE.addEventListener('click', () => {
 
     listaDeMusica.scrollBy({ left: -300, behavior: 'smooth'})
+
+});
+
+setaDB.addEventListener('click', () => {
+
+    listaDeArtistas.scrollBy({ left: 300, behavior: 'smooth'})
+
+});
+
+setaEB.addEventListener('click', () => {
+
+    listaDeArtistas.scrollBy({ left: -300, behavior: 'smooth'})
 
 });
